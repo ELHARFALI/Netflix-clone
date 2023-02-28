7a2191b5130e10fd5b82a19cb47c2f2063669828
Elharfali <elharfali.youssef.20@gmail.com>
2023-02-28 12:16:54 +0100

    Add Navbar, Fetch data from api, and Add Hero to Main component

---
+++ src/Requests.js
@@ -0,0 +1,9 @@
+const key = 'f41e473c85643a905f7eb9da69dc9a2a'
+
+export const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+}
